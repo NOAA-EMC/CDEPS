c4583cf31b49eb32bd610f709bced834603eddcd
Bin.Li <bin.li@noaa.gov>
2021-01-26 19:42:28 +0000

    Updated atm_comp_nuopc.F90,datm_datamode_cfsr_mod.F90 and CMakeLists.txt.

--- datm/atm_comp_nuopc.F90
+++ datm/atm_comp_nuopc.F90
@@ -65,6 +65,12 @@ module atm_comp_nuopc
   use datm_datamode_era5_mod    , only : datm_datamode_era5_restart_write
   use datm_datamode_era5_mod    , only : datm_datamode_era5_restart_read
 
+  use datm_datamode_cfsr_mod    , only : datm_datamode_cfsr_advertise
+  use datm_datamode_cfsr_mod    , only : datm_datamode_cfsr_init_pointers
+  use datm_datamode_cfsr_mod    , only : datm_datamode_cfsr_advance
+  use datm_datamode_cfsr_mod    , only : datm_datamode_cfsr_restart_write
+  use datm_datamode_cfsr_mod    , only : datm_datamode_cfsr_restart_read
+
   implicit none
   private ! except
 
@@ -303,6 +309,7 @@ contains
          trim(datamode) == 'CORE_IAF_JRA' .or. &
          trim(datamode) == 'CLMNCEP'      .or. &
          trim(datamode) == 'CPLHIST'      .or. &
+         trim(datamode) == 'CFSR'         .or. &
          trim(datamode) == 'ERA5') then
     else
        call shr_sys_abort(' ERROR illegal datm datamode = '//trim(datamode))
@@ -327,6 +334,9 @@ contains
        if (ChkErr(rc,__LINE__,u_FILE_u)) return
     case ('ERA5')
        call datm_datamode_era5_advertise(exportState, fldsExport, flds_scalar_name, rc)
+       if (ChkErr(rc,__LINE__,u_FILE_u)) return
+    case ('CFSR')
+       call datm_datamode_cfsr_advertise(exportState, fldsExport, flds_scalar_name, rc)
        if (ChkErr(rc,__LINE__,u_FILE_u)) return
     end select
 
@@ -567,6 +577,9 @@ contains
        case('ERA5')
           call datm_datamode_era5_init_pointers(exportState, sdat, rc)
           if (ChkErr(rc,__LINE__,u_FILE_u)) return
+       case('CFSR')
+          call datm_datamode_cfsr_init_pointers(exportState, sdat, rc)
+          if (ChkErr(rc,__LINE__,u_FILE_u)) return
        end select
 
        ! Read restart if needed
@@ -582,6 +595,8 @@ contains
              call datm_datamode_cplhist_restart_read(restfilm, inst_suffix, logunit, my_task, mpicom, sdat)
           case('ERA5')
              call datm_datamode_era5_restart_read(restfilm, inst_suffix, logunit, my_task, mpicom, sdat)
+          case('CFSR')
+             call datm_datamode_cfsr_restart_read(restfilm, inst_suffix, logunit, my_task, mpicom, sdat)
           end select
        end if
 
@@ -629,6 +644,10 @@ contains
        call datm_datamode_era5_advance(exportstate, masterproc, logunit, mpicom, target_ymd, &
             target_tod, sdat%model_calendar, rc)
        if (ChkErr(rc,__LINE__,u_FILE_u)) return
+    case('CFSR')
+       call datm_datamode_cfsr_advance(exportstate, masterproc, logunit, mpicom, target_ymd, &
+            target_tod, sdat%model_calendar, rc)
+       if (ChkErr(rc,__LINE__,u_FILE_u)) return
     end select
 
     ! Write restarts if needed
@@ -652,6 +671,10 @@ contains
           if (ChkErr(rc,__LINE__,u_FILE_u)) return
        case('ERA5')
           call datm_datamode_era5_restart_write(case_name, inst_suffix, target_ymd, target_tod, &
+               logunit, my_task, sdat)
+          if (ChkErr(rc,__LINE__,u_FILE_u)) return
+       case('CFSR')
+          call datm_datamode_cfsr_restart_write(case_name, inst_suffix, target_ymd, target_tod, &
                logunit, my_task, sdat)
           if (ChkErr(rc,__LINE__,u_FILE_u)) return
        end select
